5c5d93407e26601c95bb5751c928f58cc8257545
Garima Purohit <purohitgarima2307@gmail.com>
2026-07-07 15:02:18 +0530

    added condition in the code

--- backend/db/initDB.js
+++ backend/db/initDB.js
@@ -1,23 +1,25 @@
 const db = require("./db");
 
 console.log("initDB file executed");
+db.run("PRAGMA foreign_keys = ON;");
+
+// db.run("DROP TABLE IF EXISTS meter_readings;");
+// db.run("DROP TABLE IF EXISTS users;");
+// db.run("DROP TABLE IF EXISTS otp_codes;");
 
 db.run(`
-CREATE TABLE IF NOT EXISTS meter_readings (
-    RegisterId INTEGER PRIMARY KEY AUTOINCREMENT,
-    caNumber TEXT NOT NULL,
-    meterNumber TEXT NOT NULL,
-    readingDate TEXT NOT NULL,
-    kwh REAL NOT NULL,
-    kvh REAL NOT NULL,
-    imageName TEXT,
-    createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS otp_codes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL,
+    otp TEXT NOT NULL,
+    expiresAt DATETIME NOT NULL,
+    verified INTEGER DEFAULT 0
 )
 `, (err) => {
     if (err) {
-        console.error("Error MeterReading tablee:", err);
+        console.error("error otpcodes table:", err);
     } else {
-        console.log("meter Reading table.. working");
+        console.log("otpcodes table workinggg");
     }
 });
 
@@ -37,19 +39,22 @@ CREATE TABLE IF NOT EXISTS users (
     }
 });
 
-// Authentication: OTP codes table for email verification
 db.run(`
-CREATE TABLE IF NOT EXISTS otp_codes (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT NOT NULL,
-    otp TEXT NOT NULL,
-    expiresAt DATETIME NOT NULL,
-    verified INTEGER DEFAULT 0
+CREATE TABLE IF NOT EXISTS meter_readings (
+    RegisterId INTEGER PRIMARY KEY AUTOINCREMENT,
+    caNumber TEXT NOT NULL,
+    meterNumber TEXT NOT NULL,
+    readingDate TEXT NOT NULL,
+    kwh REAL NOT NULL,
+    kvh REAL NOT NULL,
+    imageName TEXT,
+    createdAt DATETIME DEFAULT CURRENT_TIMEnpmSTAMP,
+    FOREIGN KEY (caNumber) REFERENCES users (caNumber) ON DELETE CASCADE
 )
 `, (err) => {
     if (err) {
-        console.error("error otpcodes table:", err);
+        console.error("Error MeterReading tablee:", err);
     } else {
-        console.log("otpcodes table workinggg");
+        console.log("meter Reading table.. working");
     }
 });
